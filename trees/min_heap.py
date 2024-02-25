34aced0ff068e936ad46675ebbadce851f273fe4
Jack Yao <magicyao22@icloud.com>
2024-02-25 11:33:12 +0800

    Improved heaps efficiency.

--- trees/min_heap.py
+++ trees/min_heap.py
@@ -1,4 +1,3 @@
-import math
 from copy import deepcopy
 
 
@@ -10,14 +9,13 @@ class MinHeap:
         self.items_list.extend(items)
 
     def build_heap(self):  # Ensure the min item is at root.
-        total_subtrees = math.floor(len(self.items_list) / 2)
-        for i in reversed(range(total_subtrees)):  # Subtrees iterate from bottom to top.
+        total_subtrees = len(self.items_list) // 2
+        for i in range(total_subtrees - 1, -1, -1):  # Subtrees iterate from bottom to top.
             self.heapify(root_idx=i)
 
     def add_items(self, items, reset=False):
         if isinstance(items, dict):
             return
-
         if reset:
             self.items_list.clear()
 
@@ -45,8 +43,7 @@ class MinHeap:
                     self.items_list[swap_idx], self.items_list[root_idx])
 
                 # Heapify special case: if subtree has both children and does a swap.
-                # Jump to the subtree at swap index to ensure relative order.
-                self.heapify(root_idx=swap_idx)
+                self.heapify(root_idx=swap_idx)  # Jump to the subtree at swap index to ensure relative order.
             return
 
         if 2 * root_idx + 1 < len(self.items_list):  # If just left child exists.
@@ -73,48 +70,33 @@ class MinHeap:
         self.items_list.extend(copied_items_list)  # Restoration.
         return sorted_list
 
-    def find_min_value(self, remove=False):
+    def find_min(self, remove=False):
         if remove:
             root_value = self.items_list.pop(0)
             self.build_heap()  # Ensure the min item is still at root.
             return root_value
         return self.items_list[0]
 
-    def find_median_value(self):
-        if len(self.items_list) == 0:
+    def find_median(self):
+        total_items = len(self.items_list)
+        if total_items == 0:
             return None
-        if len(self.items_list) == 1:
+        if total_items == 1:
             return self.items_list[0]
 
-        copied_items_list = deepcopy(self.items_list)  # Deep copy self.items list for later restoration.
-        total_items = len(self.items_list)
-        # Median is defined by rank from smallest to largest.
-        median_depth = (total_items + 1) // 2 if total_items % 2 == 1 else total_items // 2
-
-        self.build_heap()  # Build items list into min heap structure before sorting.
-        for i in range(median_depth):
-            # At the start of each iteration, switch the last item with root (min item).
-            self.items_list[0], self.items_list[-1] = self.items_list[-1], self.items_list[0]
-
-            if i + 1 == median_depth:  # After popping out first (median - 1) items, the next one is median.
-                median = self.items_list.pop(-1)
-                self.items_list.clear()  # Clear and restore.
-                self.items_list.extend(copied_items_list)
-                return median
-
-            self.items_list.pop(-1)
-            self.heapify()  # Heapify for the next iteration.
+        sorted_list = self.sort()  # Sort from "low to high".
+        # When items count is even, median is defined as the (count / 2) smallest item.
+        median_idx = total_items // 2 if total_items % 2 == 1 else (total_items // 2) - 1
+        return sorted_list[median_idx]
 
 
 if __name__ == '__main__':
     import time
 
-    start_time = time.time()
-    numbers_list = [i for i in reversed(range(1, 100001))]
+    numbers_list = [i for i in range(1000000, 0, -1)]
     min_heap = MinHeap(numbers_list)
 
-    assert min_heap.sort() == sorted(numbers_list)
-    end_time = time.time()
+    start_time = time.time()
     print(f'Sorted list:\n{min_heap.sort()}\n')
+    end_time = time.time()
     print(f'Total runtime: {str(round(end_time - start_time, 2))} seconds on {len(numbers_list)} items.\n')
-    print(f'(From smallest to biggest) Median item: {str(min_heap.find_median_value())}.')
