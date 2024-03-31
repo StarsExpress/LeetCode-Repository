bf53da3c06c9a3e32bbb5455a7755003e0531f08
Jack Yao <magicyao22@icloud.com>
2024-03-31 22:22:44 +0800

    Added comments.

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -11,31 +11,31 @@ def quick_sort_list(input_array: list | tuple | set, pivot_choice: str = 'median
             pivot = input_array[-1]
 
         else:  # Median of the three (1st item, middle item, last item) method.
-            median_of_3_list = [input_array[0], input_array[-1]]
+            medians = [input_array[0], input_array[-1]]
             if len(input_array) % 2 == 1:
-                median_of_3_list.append(input_array[len(input_array) // 2])
+                medians.append(input_array[len(input_array) // 2])
 
             else:
-                median_of_3_list.append(input_array[len(input_array) // 2 - 1])
+                medians.append(input_array[len(input_array) // 2 - 1])
 
-            median_of_3_list.sort()
-            pivot = median_of_3_list[1]
-            del median_of_3_list
+            medians.sort()
+            pivot = medians.pop(1)
+            del medians
 
         pivot_idx = input_array.index(pivot)
         input_array[0], input_array[pivot_idx] = input_array[pivot_idx], input_array[0]
         del pivot_idx
 
-    front_idx, back_idx = 1, 1  # Both front and back indices start at 2nd item.
+    front_idx, back_idx = 1, 1  # Both front and back indices start at 2nd item to "ensure 2nd item <= pivot".
 
-    while front_idx < len(input_array):  # Once front index reaches the end, break while.
+    while front_idx < len(input_array):  # Until front index reaches the end.
         if input_array[front_idx] < pivot:  # Switch items at front and back indices.
             input_array[back_idx], input_array[front_idx] = input_array[front_idx], input_array[back_idx]
-            back_idx += 1  # Only increment back index when swap happens.
+            back_idx += 1  # Whenever a switch happens, increment back index.
 
         front_idx += 1  # Always increment front index.
 
-    # Switch pivot with the item at back index - 1.
+    # Switch pivot with item at back index - 1.
     input_array[0], input_array[back_idx - 1] = input_array[back_idx - 1], input_array[0]
 
     input_array[:back_idx - 1], back_comparisons_count = quick_sort_list(input_array[:back_idx - 1], pivot_choice)
