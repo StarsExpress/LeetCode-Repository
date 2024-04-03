48673622c451d4c2a4bebdd749c5e38638898100
Jack Yao <magicyao22@icloud.com>
2024-04-03 15:13:54 +0800

    Completed cards revelation.

---
+++ sorting/cards_revelation.py
@@ -0,0 +1,16 @@
+
+def reveal_cards(deck: list[int]):  # LeetCode Q.950.
+    if len(deck) <= 1:
+        return deck
+
+    deck.sort()  # Sort from smallest to biggest.
+    if len(deck) == 2:
+        return deck
+
+    desired_deck, idx = deck[-2:], -3  # Iteration starts from the 3rd biggest card.
+    while True:
+        if len(desired_deck) == len(deck):
+            return desired_deck
+        desired_deck.insert(0, deck[idx])
+        desired_deck.insert(1, desired_deck.pop(-1))
+        idx -= 1
