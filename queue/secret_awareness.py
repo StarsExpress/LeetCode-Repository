3745c8e0a3c7489a1e67fae73e60a355697cf1b2
Jack Yao <magicyao22@icloud.com>
2024-04-07 21:15:51 +0800

    Enhanced efficiency.

--- queue/secret_awareness.py
+++ queue/secret_awareness.py
@@ -5,20 +5,19 @@ def count_secret_awareness(days: int, delay: int, forget: int):  # LeetCode Q.23
     if not days >= forget > delay:
         raise ValueError('Relationship of days >= forget > delay must hold.')
 
-    awareness, current_day = 1, 2  # Iteration starts at "day 2's beginning".
+    current_day = 2  # Iteration starts at "day 2's beginning".
     delay_queue = [0] * (delay - 1)
     delay_queue.append(1)  # Already 1 person knows secret at day 2's start.
     memory_queue = [0] * (forget - delay)
 
     while True:
-        awareness -= memory_queue.pop(0)  # Step 1: awareness drops as some people forget secret.
+        memory_queue.pop(0)  # Step 1: some people forget secret at current day's beginning.
 
         memory_queue.append(delay_queue.pop(0))  # Step 2: move people who are off delay queue to memory queue.
 
         delay_queue.append(sum(memory_queue))  # Step 3: all people in memory queue can spread secret.
 
-        awareness += sum(memory_queue) + sum(delay_queue) - awareness  # Step 4: update awareness count.
-
         current_day += 1
         if current_day > days:
-            return awareness % (10 ** 9 + 7)  # Prevent too big result.
+            # Step 4: calculate awareness count. Prevent too big result.
+            return (sum(memory_queue) + sum(delay_queue)) % (10 ** 9 + 7)
