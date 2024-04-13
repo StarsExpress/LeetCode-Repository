07bbb146e2038fdff72309fac36a7dcb6a01e964
Jack Yao <magicyao22@icloud.com>
2024-04-13 17:55:55 +0800

    Completed reverse pairs.

---
+++ search/reverse_pairs.py
@@ -0,0 +1,45 @@
+
+def count_smaller_rights(target: int, sorted_integers: list[int] | tuple[int]):
+    if len(sorted_integers) <= 0:
+        return 0
+
+        # Insertion idx: target's idx into sorted integers to maintain order.
+    back_idx, front_idx, insertion_idx = 0, len(sorted_integers) - 1, 0
+    while True:  # First while: search for insertion idx.
+        if back_idx > front_idx:
+            insertion_idx = back_idx
+            break
+
+        mid_idx = (back_idx + front_idx) // 2
+        if sorted_integers[mid_idx] < target:
+            back_idx = mid_idx + 1
+            continue
+        front_idx = mid_idx - 1
+
+    back_idx, front_idx = 0, len(sorted_integers) - 1
+    while True:  # Second while: count number of ints < 0.5 * target.
+        if back_idx > front_idx:
+            return back_idx, insertion_idx  # Back idx implies number of ints < 0.5 * target.
+
+        mid_idx = (back_idx + front_idx) // 2
+        if 2 * sorted_integers[mid_idx] < target:
+            back_idx = mid_idx + 1
+            continue
+        front_idx = mid_idx - 1
+
+
+def count_reverse_pairs(integers: list[int]):  # LeetCode Q.493.
+    if len(integers) <= 1:
+        return 0
+
+    reverse_pairs = 0
+    sorted_integers = [integers.pop(-1)]  # Start from rightmost int.
+
+    while True:
+        if len(integers) <= 0:
+            return reverse_pairs
+
+        popped_integer = integers.pop(-1)
+        smaller_right, insertion_idx = count_smaller_rights(popped_integer, sorted_integers)
+        reverse_pairs += smaller_right
+        sorted_integers.insert(insertion_idx, popped_integer)
