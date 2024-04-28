8830822ffc53d122c52c18b5f2e6cc615fe81014
Jack Yao <105488074+StarsExpress@users.noreply.github.com>
2024-04-28 17:23:20 +0800

    Completed stock span.

---
+++ stack/stock_span.py
@@ -0,0 +1,21 @@
+
+class StockSpanner:  # LeetCode Q.901.
+
+    def __init__(self):
+        self.stock_stack = []
+
+    def find_span(self, price: int):
+        stack_size = len(self.stock_stack)
+        if stack_size <= 0:
+            self.stock_stack.append((price, 1))
+            return 1
+
+        stack_idx, past_span = -1, 0
+        while stack_idx >= -stack_size:
+            if self.stock_stack[stack_idx][0] > price:
+                break
+            past_span += self.stock_stack[stack_idx][1]
+            stack_idx -= self.stock_stack[stack_idx][1]
+
+        self.stock_stack.append((price, past_span + 1))
+        return past_span + 1
