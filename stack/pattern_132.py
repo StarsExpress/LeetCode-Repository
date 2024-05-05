3b31a0e167e80d0579fa307bc31325656c70e7c2
Jack Yao <magicyao22@icloud.com>
2024-05-05 16:55:59 +0800

    Completed pattern 132.

---
+++ stack/pattern_132.py
@@ -0,0 +1,17 @@
+
+def find_132_pattern(integers: list[int]):  # LeetCode Q.456.
+    stack = []  # Format: (interval min, interval max).
+    for integer in integers:
+        if not stack or integer < stack[-1][0]:  # When stack is empty or current int < interval min.
+            stack.append([integer, integer])
+            continue
+
+        interval_min = stack[-1][0]  # Record interval min for later interval storage.
+        while stack and stack[-1][1] < integer:  # Keep popping as long as interval max < current int.
+            stack.pop()
+
+        if stack and stack[-1][0] < integer < stack[-1][1]:
+            return True
+        stack.append([interval_min, integer])
+
+    return False
