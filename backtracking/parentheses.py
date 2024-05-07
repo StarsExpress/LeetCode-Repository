d12c3897a09ddb60a6947ee092da8f46584a7e8e
Jack Yao <105488074+StarsExpress@users.noreply.github.com>
2024-05-07 16:59:14 +0800

    Completed parentheses.

---
+++ backtracking/parentheses.py
@@ -0,0 +1,18 @@
+
+def generate_parentheses(pairs: int):  # LeetCode Q.22.
+    parentheses = []
+
+    # Track counts of left side and right side parenthesis.
+    def recursion(left_count, right_count, parenthesis):
+        if left_count == right_count == pairs:  # Base case: reach max number of parentheses pairs.
+            parentheses.append(parenthesis)
+            return
+
+        if left_count < pairs:  # Add left parenthesis if its count < pairs.
+            recursion(left_count + 1, right_count, parenthesis + '(')
+
+        if left_count > right_count:  # Add right parenthesis if its count < that of left.
+            recursion(left_count, right_count + 1, parenthesis + ')')
+
+    recursion(0, 0, '')
+    return parentheses
