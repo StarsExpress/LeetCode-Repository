a99036346bb83d007e5b690d5f4d6c5408c7f7d4
Jack Yao <magicyao22@icloud.com>
2024-11-03 20:24:49 +0800

    Improved several problems' efficiency.

--- bit_manipulation/three_equal_parts.py
+++ bit_manipulation/three_equal_parts.py
@@ -1,46 +1,52 @@
 
-def split_triplets(numbers: list[int]) -> list[int]:  # LeetCode Q.927.
-    total_sum = sum(numbers)
+def split_triplets(nums: list[int]) -> list[int]:  # LeetCode Q.927.
+    total_sum = sum(nums)
     if total_sum % 3 != 0:  # Base case.
         return [-1, -1]
     if total_sum == 0:  # Base case.
         return [0, 2]
 
-    total_nums, one_third = len(numbers), total_sum // 3
+    total_nums, one_third = len(nums), total_sum // 3
     encountered_ones, ending_zeros = 0, 0
-    first_idx, second_idx, third_idx = None, None, None
+    num_1_last_1_idx, num_2_first_1_idx = None, None
+    num_2_last_1_idx, num_3_first_1_idx = None, None
 
-    for idx, number in enumerate(numbers):
-        if number == 1:
+    for idx, num in enumerate(nums):
+        if num == 1:
             encountered_ones += 1
 
-        if encountered_ones == one_third and first_idx is None:  # 1st separation.
-            first_idx = idx
+            if encountered_ones == one_third:
+                num_1_last_1_idx = idx
 
-        if encountered_ones == 2 * one_third and second_idx is None:  # 2nd separation.
-            second_idx = idx + 1
+            if encountered_ones == one_third + 1:
+                num_2_first_1_idx = idx
 
-        if encountered_ones == 2 * one_third + 1 and third_idx is None:
-            third_idx = idx
+            if encountered_ones == 2 * one_third:
+                num_2_last_1_idx = idx
 
-        if encountered_ones == total_sum:  # Location of the last 1.
-            ending_zeros += total_nums - 1 - idx
-            break
+            if encountered_ones == 2 * one_third + 1:
+                num_3_first_1_idx = idx
 
-    if second_idx - 1 + ending_zeros >= third_idx:
-        return [-1, -1]
-    second_idx += ending_zeros
+            if encountered_ones == total_sum:  # Location of the last 1.
+                ending_zeros += total_nums - 1 - idx
+                break
 
-    first_idx += ending_zeros
-    if first_idx >= second_idx:
-        return [-1, -1]
-
-    first_number = int("".join(str(num) for num in numbers[:first_idx + 1]), 2)
-    second_number = int("".join(str(num) for num in numbers[first_idx + 1: second_idx]), 2)
-    if first_number != second_number:
+    num_2_last_1_idx += ending_zeros
+    if num_2_last_1_idx >= num_3_first_1_idx:
         return [-1, -1]
 
-    third_number = int("".join(str(num) for num in numbers[second_idx:]), 2)
-    if second_number != third_number:
+    num_1_last_1_idx += ending_zeros
+    if num_1_last_1_idx >= num_2_first_1_idx:
         return [-1, -1]
-    return [first_idx, second_idx]
+
+    split_1_idx, split_2_idx = num_1_last_1_idx, num_2_last_1_idx + 1
+
+    num_1 = int("".join(str(num) for num in nums[:split_1_idx + 1]), 2)
+    num_2 = int("".join(str(num) for num in nums[split_1_idx + 1: split_2_idx]), 2)
+    if num_1 != num_2:
+        return [-1, -1]
+
+    num_3 = int("".join(str(num) for num in nums[split_2_idx:]), 2)
+    if num_2 != num_3:
+        return [-1, -1]
+    return [split_1_idx, split_2_idx]
