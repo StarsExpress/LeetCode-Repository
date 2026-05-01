bc4895ef07feb9b9f9bae806baf3646768d75000
Jack Yao <105488074+StarsExpress@users.noreply.github.com>
2026-05-01 11:17:44 -0400

    Added Q.76 directory.

---
+++ sliding_window/0076_hard/min_window_substring.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+string findMinWindowSubstring(string sourceString, string targetString) // LeetCode Q.76.
+{
+    unordered_map<char, int> tgtCharsCounts;
+
+    for (auto character : targetString)
+        tgtCharsCounts[character]++;
+
+    int uniqueTgtCharsCount = tgtCharsCounts.size();
+
+    // Must not exceed unique target chars count during sliding window.
+    int coverage = 0;
+
+    int minWindowLeftIdx = -1;
+    int minLen = sourceString.size() + 1; // Source length + 1: symbol of not found yet.
+
+    int leftIdx = 0;
+    for (int rightIdx = 0; rightIdx < sourceString.size(); rightIdx++)
+    {
+        char character = sourceString[rightIdx];
+        if (tgtCharsCounts.find(character) != tgtCharsCounts.end())
+        {
+            tgtCharsCounts[character]--;
+
+            if (tgtCharsCounts[character] == 0) // Becomes covered.
+                coverage++;
+        }
+
+        while (coverage == uniqueTgtCharsCount) // Can try to shrink window.
+        {
+            int windowLen = rightIdx + 1 - leftIdx;
+
+            if (windowLen < minLen)
+                minLen = windowLen, minWindowLeftIdx = leftIdx;
+
+            char removedChar = sourceString[leftIdx];
+            if (tgtCharsCounts.find(removedChar) != tgtCharsCounts.end())
+            {
+                tgtCharsCounts[removedChar]++;
+
+                if (tgtCharsCounts[removedChar] > 0) // Becomes uncovered.
+                    coverage--;
+            }
+
+            leftIdx += 1;
+        }
+    }
+
+    if (minLen == sourceString.size() + 1)
+        return "";
+
+    return sourceString.substr(minWindowLeftIdx, minLen);
+}
---
+++ sliding_window/0076_hard/min_window_substring.py
@@ -0,0 +1,44 @@
+
+def find_min_window_substring(source_str: str, target_str: str) -> str:  # LeetCode Q.76.
+    tgt_chars_counts: dict[str, int] = dict()
+
+    for char in target_str:
+        if char not in tgt_chars_counts.keys():
+            tgt_chars_counts.update({char: 0})
+        tgt_chars_counts[char] += 1
+
+    unique_tgt_chars_count = len(tgt_chars_counts)
+
+    # Must not exceed unique target chars count during sliding window.
+    coverage = 0
+
+    min_window_left_idx = -1
+    min_len = len(source_str) + 1  # Source length + 1: symbol of not found yet.
+
+    left_idx = 0
+    for right_idx, char in enumerate(source_str):
+        if char in tgt_chars_counts.keys():
+            tgt_chars_counts[char] -= 1
+
+            if tgt_chars_counts[char] == 0:  # Becomes covered.
+                coverage += 1
+
+        while coverage == unique_tgt_chars_count:  # Can try to shrink window.
+            window_len = right_idx + 1 - left_idx
+
+            if window_len < min_len:
+                min_len = window_len
+                min_window_left_idx = left_idx
+
+            removed_char = source_str[left_idx]
+            if removed_char in tgt_chars_counts.keys():
+                tgt_chars_counts[removed_char] += 1
+
+                if tgt_chars_counts[removed_char] > 0:  # Becomes uncovered.
+                    coverage -= 1
+
+            left_idx += 1
+
+    if min_len == len(source_str) + 1: return ""
+
+    return source_str[min_window_left_idx: min_window_left_idx + min_len]
